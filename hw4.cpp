eff423e763b8e41e730920522e0cbde4fdd112c6
Brandon Marquez <bjm241@txstate.edu>
2020-09-28 21:28:31 +0000

    in rotate, print n and list.

--- hw4.cpp
+++ hw4.cpp
@@ -125,6 +125,7 @@ static list_t rotate_helper(list_t list, unsigned int n, list_t result) {
 
 list_t rotate(list_t list, unsigned int n){
    list_print(list);
+   std::cout << std::endl << n << std::endl;
    return rotate_helper(list, n, list_make());
 }
 
