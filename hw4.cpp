528c21672fa582d6e2093b38cc24ac813a58d769
Brandon Marquez <bjm241@txstate.edu>
2020-09-29 03:58:32 +0000

    Filter & fib.

--- hw4.cpp
+++ hw4.cpp
@@ -26,26 +26,7 @@ int product(list_t list) {
   return product_helper(list, 1);
 }
 
-int accumulate(list_t l, int (*fn)(int, int), int identity)
-{
-    /*
-    // REQUIRES: fn must be associative.
-    // EFFECTS: return identity if list is empty
-    //          return fn(list_first(list), 
-    //                    accumulate(list_rest(list),
-    //                               fn, identity) otherwise.
-    //
-    // For example, if you have the following function:
-    //
-    //           int add(int x, int y);
-    //
-    // Then the following invocation returns the sum of all elements:
-    //
-    //           accumulate(list, add, 0);
-    //
-    // The "identity" argument is typically the value for which 
-    // fn(X, identity) == X, for any X.
-    */ 
+int accumulate(list_t l, int (*fn)(int, int), int identity) {
   if (list_isEmpty(l)) {
     return identity;
   } else {
@@ -107,12 +88,17 @@ list_t filter_even(list_t list) {
    return filter_even_helper(list, list_make());
 }
 
+static list_t filter_helper(list_t list, bool (*fn)(int), list_t result) {
+  if(fn) {
+    return filter_helper(list_rest(list), fn, list_make(list_first(list), result));
+  }
+  else { 
+    return filter_helper(list_rest(list), fn, result);
+  }
+}
+
 list_t filter(list_t list, bool (*fn)(int)){
-    /*
-    // EFFECTS: returns a list containing precisely the elements of list
-    //          for which the predicate fn() evaluates to true, in the
-    //          order in which  they appeared in list.
-    */
+    return filter_helper(list, fn, list_make());
 }
 
 static list_t rotate_helper(list_t list, unsigned int n, list_t result) {
@@ -125,24 +111,17 @@ static list_t rotate_helper(list_t list, unsigned int n, list_t result) {
   }
 }
 
-list_t rotate(list_t list, unsigned int n){
-  list_print(list);
-  list_print(rotate_helper(list, n, list_make()));
-  std::cout << n << std::endl;
+list_t rotate(list_t list, unsigned int n) {
    return rotate_helper(list, n, list_make());
 }
 
 static list_t insert_list_helper(list_t first, list_t second, unsigned int n, list_t result) {
-  if (list_isEmpty(first)) {
-    return reverse(result);
-  }else if (n > 0 || list_isEmpty(second)) {
-    return insert_list_helper(list_rest(first), second, n-1, list_make(list_first(first), result));
-  } else {
-    return insert_list_helper(first, list_rest(second), n, list_make(list_first(second), result));
-  }
+  return result;
 }
 
 list_t insert_list(list_t first, list_t second, unsigned int n){
+  list_t first_reversed = reverse(first);
+  list_t second_reversed = reverse(second);
    return insert_list_helper(first, second, n, list_make());
 }
 
@@ -155,11 +134,6 @@ static list_t chop_helper(list_t l, unsigned int n, list_t result) {
 }
 
 list_t chop(list_t l, unsigned int n){
-    /*
-    // REQUIRES l has at least n elements
-    // EFFECTS: returns the list equal to l without its last n
-    //          elements
-    */
    list_t list_reversed = reverse(l);
    return chop_helper(list_reversed, n, list_make());
 }
@@ -173,6 +147,15 @@ int fib(int n){
     //          fib(n) = fib(n-1) + fib(n-2) for (n>1).
     // This must be recursive but need not be tail recursive
     */
+    return fib(n-1) + fib(n-2);
+}
+
+static int fib_tail_helper(int n, int fib0, int fib1) {
+  if(n < 0) {
+    return result;
+  } else {
+    return fib_tail_helper(n-1, fib0 + fib1, fib0 * 2 + fib1);
+  }
 }
 
 int fib_tail(int n){
@@ -186,4 +169,5 @@ int fib_tail(int n){
     // Hint: instead of starting at n and workind down, start at
     // 0 and 1 and *upwards*.
     */
+    return fib_tail_helper(n, 0, 1);
 }
